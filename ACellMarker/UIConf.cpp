8047a3ae7df710c317af5da4cfb7aa9f4d695b43
乔 <qiaojx1@qq.com>
2020-02-12 17:11:07 +0800

    Add toolbar and central widget

--- ACellMarker/UIConf.cpp
+++ ACellMarker/UIConf.cpp
@@ -19,4 +19,27 @@ void InitMainWindow(QMainWindow& appMain, QApplication& app)
 	editMenu = appMenuBar->addMenu("Edit");
 	editMenu->addAction("Undo");
 	editMenu->addAction("Redo");
+
+	QWidget* centralWidget = new QWidget();
+	appMain.setCentralWidget(centralWidget);
+	centralWidget->setGeometry(0, 0, 400, 400);
+
+	QPushButton* button = new QPushButton();
+	button->setText("aaa");
+	button->setParent(centralWidget);
+
+	QToolBar* appToolBar = new QToolBar("MainToolBar",nullptr);
+	appMain.addToolBar(Qt::LeftToolBarArea,appToolBar);
+	appToolBar->setAllowedAreas(Qt::LeftToolBarArea);
+	appToolBar->setIconSize(QSize(M_ICONSIZE, M_ICONSIZE));
+	appToolBar->setMinimumWidth(M_ICONSIZE);
+	appToolBar->setMaximumWidth(2 * M_ICONSIZE + M_ICONSPACE);
+	appToolBar->setMovable(false);
+	appToolBar->setOrientation(Qt::Vertical);
+	appToolBar->addAction(QIcon("../res/icons/test.png"), "Test Icon");
+	appToolBar->addAction(QIcon("../res/icons/test.png"), "Test Icon2");
+	appToolBar->addSeparator();
+	appToolBar->addAction(QIcon("../res/icons/test.png"), "Test Icon3");
+	appToolBar->addAction(QIcon("../res/icons/test.png"), "Test Icon4");
+
 }
