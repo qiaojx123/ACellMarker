9ea0635cc27b35cc7a8985377003d3e7f097b2fe
乔 <qiaojx1@qq.com>
2020-02-13 16:43:27 +0800

    Rearrange the init of menubar and toolbar with macros.

--- ACellMarker/UIConf.cpp
+++ ACellMarker/UIConf.cpp
@@ -2,44 +2,109 @@
 
 void InitMainWindow(QMainWindow& appMain, QApplication& app)
 {
-	appMain.setMinimumSize(QSize(512, 384));
+	appMain.setMinimumSize(QSize(M_MWINDOWWID, M_MWINDOWHEI));
 
 	QMenuBar* appMenuBar;
 	appMenuBar = appMain.menuBar();
 
-	QMenu *fileMenu, *editMenu;
-	fileMenu = appMenuBar->addMenu("File");
-	fileMenu->addAction("Open");
-	fileMenu->addAction("Save");
-	fileMenu->addAction("Save As");
-	fileMenu->addSeparator();
-	fileMenu->addAction("Quit");
-	QObject::connect(fileMenu->actions().at(4), SIGNAL(triggered()), &app, SLOT(quit()));
+	QMenu *fileMenu, *editMenu, *helpMenu;
 
-	editMenu = appMenuBar->addMenu("Edit");
-	editMenu->addAction("Undo");
-	editMenu->addAction("Redo");
+	//Init file menu.
+	fileMenu = InitFileMenu();
+	appMenuBar->addMenu(fileMenu);
+	QObject::connect(fileMenu->actions().at(MENU_FILE_QUIT), SIGNAL(triggered()), &app, SLOT(quit()));
+
+	//Init edit menu.
+	editMenu = InitEditMenu();
+	appMenuBar->addMenu(editMenu);
+
+	//Init help Menu.
+	helpMenu = InitHelpMenu();
+	appMenuBar->addMenu(helpMenu);
 
 	QWidget* centralWidget = new QWidget();
 	appMain.setCentralWidget(centralWidget);
-	centralWidget->setGeometry(0, 0, 400, 400);
+//	centralWidget->setGeometry(0, 0, 400, 400);
+	centralWidget->setMinimumSize(M_MWINDOWWID - M_ICONSIZE, M_MWINDOWHEI);
 
 	QPushButton* button = new QPushButton();
 	button->setText("aaa");
 	button->setParent(centralWidget);
 
-	QToolBar* appToolBar = new QToolBar("MainToolBar",nullptr);
-	appMain.addToolBar(Qt::LeftToolBarArea,appToolBar);
-	appToolBar->setAllowedAreas(Qt::LeftToolBarArea);
-	appToolBar->setIconSize(QSize(M_ICONSIZE, M_ICONSIZE));
-	appToolBar->setMinimumWidth(M_ICONSIZE);
-	appToolBar->setMaximumWidth(2 * M_ICONSIZE + M_ICONSPACE);
-	appToolBar->setMovable(false);
-	appToolBar->setOrientation(Qt::Vertical);
-	appToolBar->addAction(QIcon("../res/icons/test.png"), "Test Icon");
-	appToolBar->addAction(QIcon("../res/icons/test.png"), "Test Icon2");
-	appToolBar->addSeparator();
-	appToolBar->addAction(QIcon("../res/icons/test.png"), "Test Icon3");
-	appToolBar->addAction(QIcon("../res/icons/test.png"), "Test Icon4");
+	QToolBar* appToolBar;
+	appToolBar = InitToolBar();
+	appMain.addToolBar(Qt::LeftToolBarArea, appToolBar);
+	QObject::connect(appToolBar->actions().at(TOOLBAR_TEST3), SIGNAL(triggered()), &app, SLOT(quit()));
+}
 
+QMenu* InitFileMenu()
+{
+	QMenu* fileMenu = new QMenu();
+	std::string menulist[MENU_FILE_TOTAL];
+	menulist[MENU_FILE_OPENIMG	] = MENU_FILE_OPENIMG_T;
+	menulist[MENU_FILE_OPENMARK	] = MENU_FILE_OPENMARK_T;
+	menulist[MENU_FILE_QUIT		] = MENU_FILE_QUIT_T;
+	menulist[MENU_FILE_SAVE		] = MENU_FILE_SAVE_T;
+	menulist[MENU_FILE_SAVEAS	] = MENU_FILE_SAVEAS_T;
+
+	fileMenu->setTitle(MENU_FILE_TITLE);
+	for (int i = 0; i < MENU_FILE_TOTAL; ++i)
+		if (menulist[i] == "") fileMenu->addSeparator();
+		else fileMenu->addAction(menulist[i].c_str());
+
+	return fileMenu;
+}
+
+QMenu* InitEditMenu()
+{
+	QMenu* editMenu = new QMenu();
+	std::string menulist[MENU_EDIT_TOTAL];
+	menulist[MENU_EDIT_UNDO] = MENU_EDIT_UNDO_T;
+	menulist[MENU_EDIT_REDO] = MENU_EDIT_REDO_T;
+
+	editMenu->setTitle(MENU_EDIT_TITLE);
+	for (int i = 0; i < MENU_EDIT_TOTAL; ++i)
+		if (menulist[i] == "") editMenu->addSeparator();
+		else editMenu->addAction(menulist[i].c_str());
+
+	return editMenu;
+}
+
+QMenu* InitHelpMenu()
+{
+	QMenu* helpMenu = new QMenu();
+	std::string menulist[MENU_HELP_TOTAL];
+	menulist[MENU_HELP_HELP] = MENU_HELP_HELP_T;
+	menulist[MENU_HELP_ABOUT] = MENU_HELP_ABOUT_T;
+
+	helpMenu->setTitle(MENU_HELP_TITLE);
+	for (int i = 0; i < MENU_HELP_TOTAL; ++i)
+		if (menulist[i] == "") helpMenu->addSeparator();
+		else helpMenu->addAction(menulist[i].c_str());
+
+	return helpMenu;
+}
+
+QToolBar* InitToolBar()
+{
+	QToolBar* toolBar = new QToolBar();
+	std::string toollist[TOOLBAR_TOTAL], iconlist[TOOLBAR_TOTAL];
+	toollist[TOOLBAR_TEST1] = TOOLBAR_TEST1_T;
+	toollist[TOOLBAR_TEST2] = TOOLBAR_TEST2_T;
+	toollist[TOOLBAR_TEST3] = TOOLBAR_TEST3_T;
+	iconlist[TOOLBAR_TEST1] = TOOLBAR_TEST1_I;
+	iconlist[TOOLBAR_TEST2] = TOOLBAR_TEST2_I;
+	iconlist[TOOLBAR_TEST3] = TOOLBAR_TEST3_I;
+
+	toolBar->setAllowedAreas(Qt::LeftToolBarArea);
+	toolBar->setIconSize(QSize(M_ICONSIZE, M_ICONSIZE));
+	toolBar->setMinimumWidth(M_ICONSIZE);
+	toolBar->setMaximumWidth(2 * M_ICONSIZE + M_ICONSPACE);
+	toolBar->setMovable(false);
+	toolBar->setOrientation(Qt::Vertical);
+
+	for (int i = 0; i < TOOLBAR_TOTAL; ++i)
+		if (toollist[i] == "") toolBar->addSeparator();
+		else toolBar->addAction(QIcon(iconlist[i].c_str()), toollist[i].c_str());
+	return toolBar;
 }
--- ACellMarker/UIConf.h
+++ ACellMarker/UIConf.h
@@ -1,9 +1,66 @@
 #pragma once
+
+#include <string>
+
 #include "UIClasses.h"
 #include "QtWidgets/qmenubar.h"
 #include "QtWidgets/qtoolbar.h"
 
 #define M_ICONSIZE 50
 #define M_ICONSPACE 10
+#define M_MWINDOWWID 512
+#define M_MWINDOWHEI 384
+
+// Define menu items
+#if 1 
+	#define MENU_FILE_TITLE "File"
+	#define MENU_FILE_OPENIMG 0
+	#define MENU_FILE_OPENIMG_T "Open Image File"
+	#define MENU_FILE_OPENMARK 1
+	#define MENU_FILE_OPENMARK_T "Open Marklist File"
+	#define MENU_FILE_SAVE 2
+	#define MENU_FILE_SAVE_T "Save"
+	#define MENU_FILE_SAVEAS 3
+	#define MENU_FILE_SAVEAS_T "Save As"
+	#define MENU_FILE_QUIT 5
+	#define MENU_FILE_QUIT_T "Quit"
+	#define MENU_FILE_TOTAL 6
+
+	#define MENU_EDIT_TITLE "Edit"
+	#define MENU_EDIT_UNDO 0
+	#define MENU_EDIT_UNDO_T "Undo"
+	#define MENU_EDIT_REDO 1
+	#define MENU_EDIT_REDO_T "Redo"
+	#define MENU_EDIT_TOTAL 2
+	
+	#define MENU_HELP_TITLE "Help"
+	#define MENU_HELP_HELP 0
+	#define MENU_HELP_HELP_T "Help"
+	#define MENU_HELP_ABOUT 1
+	#define MENU_HELP_ABOUT_T "About"
+	#define MENU_HELP_TOTAL 2
+#endif
+
+// Define toolbar items
+#if 1
+#define TOOLBAR_TEST1 0
+#define TOOLBAR_TEST1_T "Test 1"
+#define TOOLBAR_TEST1_I	"../res/icons/test.png"
+#define TOOLBAR_TEST2 1
+#define TOOLBAR_TEST2_T "Test 2"
+#define TOOLBAR_TEST2_I	"../res/icons/test.png"
+#define TOOLBAR_TEST3 3
+#define TOOLBAR_TEST3_T "Test 3"
+#define TOOLBAR_TEST3_I	"../res/icons/test.png"
+#define TOOLBAR_TOTAL 4
+#endif
 
 void InitMainWindow(QMainWindow& appMain, QApplication& app);
+
+QMenu* InitFileMenu();
+
+QMenu* InitEditMenu();
+
+QMenu* InitHelpMenu();
+
+QToolBar* InitToolBar();
